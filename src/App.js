eba84c335b3d5901b269347243d68bc655d000db
AlexanderPolzhofer <alexander.polzhofer@gmail.com>
2022-03-25 20:45:23 +0100

    adding logo to footer component

--- src/App.js
+++ src/App.js
@@ -10,50 +10,52 @@ import Footer from './components/footer/Footer';
 
 export default function App() {
   return (
-    <AppContainer>
-      <div className={styles.blockElem}>
-        <div className={styles.blockElemElem}>
-          <h1>Paketshop Manager</h1>
-          <div className="input-group mt-3 mb-3">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Name ..."
-              aria-label="Example text with button addon"
-              aria-describedby="button-addon1">
-            </input>
+    <div>
+      <AppContainer>
+        <div className={styles.blockElem}>
+          <div className={styles.blockElemElem}>
+            <h1>Paketshop Manager</h1>
+            <div className="input-group mt-3 mb-3">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Name ..."
+                aria-label="Example text with button addon"
+                aria-describedby="button-addon1">
+              </input>
+            </div>
+            <div className="input-group mb-3">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="E-Mail ..."
+                aria-label="Example text with button addon"
+                aria-describedby="button-addon1">
+              </input>
+            </div>
+            <div className="input-group mb-3">
+              <textarea
+                className="form-control"
+                id="exampleFormControlTextarea1"
+                rows="3"
+                placeholder='Was möchten Sie uns mitteilen?'>
+              </textarea>
+            </div>
+            <button type="button" className="btn btn-success">Nachricht senden</button>
           </div>
-          <div className="input-group mb-3">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="E-Mail ..."
-              aria-label="Example text with button addon"
-              aria-describedby="button-addon1">
-            </input>
-          </div>
-          <div className="input-group mb-3">
-            <textarea
-              className="form-control"
-              id="exampleFormControlTextarea1"
-              rows="3"
-              placeholder='Was möchten Sie uns mitteilen?'>
-            </textarea>
-          </div>
-          <button type="button" className="btn btn-success">Nachricht senden</button>
+          <img
+            src={process.env.PUBLIC_URL + '/images/psm_dashboard_overlay.png'}
+            alt='dashboard-overlay-img'
+            className={styles.blockElemElem}
+            height='50%'
+            width='50%' />
         </div>
-        <img
-          src={process.env.PUBLIC_URL + '/images/psm_dashboard_overlay.png'}
-          alt='dashboard-overlay-img'
-          className={styles.blockElemElem}
-          height='50%'
-          width='50%' />
-      </div>
-      <ContentInfo />
-      <Card />
-      <Card />
-      <Accordion />
+        <ContentInfo />
+        <Card />
+        <Card />
+        <Accordion />
+      </AppContainer>
       <Footer />
-    </AppContainer>
+    </div >
   )
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,9 @@ export default function Footer() {
     return (
         <footer id="sticky-footer" className="flex-shrink-0 py-4 bg-dark text-white-50" >
             <div className="container text-center">
-                <small>Copyright &copy; Paketshop Manager</small>
+                <small>Copyright &copy; Paketshop Manager
+                    <img src={process.env.PUBLIC_URL + 'images/logo.png'} alt='logo' height='2%' width='2%' style={{marginLeft: '0.5%'}}/>
+                </small>
             </div>
         </footer>)
 }
